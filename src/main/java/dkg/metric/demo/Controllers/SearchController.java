affbb21068e41dea5e9988e16dd328cb0eeaa7c7
rafaeltavaares <rafaeltavsilva@gmail.com>
2024-10-30 12:04:15 -0300

    pronto?

--- src/main/java/dkg/metric/demo/Controllers/SearchController.java
+++ src/main/java/dkg/metric/demo/Controllers/SearchController.java
@@ -33,11 +33,12 @@ public class SearchController {
     @Autowired
     private DkgService dkgService;
 
-    @CrossOrigin(origins = "http://127.0.0.1:8081")
+    //@CrossOrigin(origins = "http://127.0.0.1:8081")
+    @CrossOrigin
     @PostMapping("/search")
     public String searchGoogle(@RequestParam String URL_CONTENT, @RequestBody SearchDTO search) throws Exception {
 
-        String requestUrl = SEARCH_URL + "?key=" + API_KEY + "&cx=" + CX + "&q=" + URL_CONTENT +"&lr=lang_pt&gl=br";
+        String requestUrl = SEARCH_URL + "?key=" + API_KEY + "&cx=" + CX + "&q=" + URL_CONTENT.replaceAll("\\s+","") +"&lr=lang_pt&gl=br";
 
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpGet request = new HttpGet(requestUrl);
---
+++ src/main/java/dkg/metric/demo/infra/configuration.java
@@ -0,0 +1,28 @@
+package dkg.metric.demo.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class configuration {
+
+    @Configuration
+    public class CorsConfig {
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("*")
+                            .allowedMethods("*")
+                            .allowedHeaders("*")
+                            .allowCredentials(true);
+                }
+            };
+        }
+    }
+
+}
